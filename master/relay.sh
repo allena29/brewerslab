326c6453082e00870b440f0fbbce99470710b781
Adam Allen <allena29@users.noreply.github.com>
2015-05-01 21:16:39 +0000

    updated paths

--- master/relay.sh
+++
@@ -1,4 +0,0 @@
-sudo kill `ps -ef | grep "sudo python pitmRelay.py" | grep -v SCREEN | grep -v grep | sed -e 's/root\s*//' -e 's/\s.*//'` 2>/dev/null
-sudo screen -dmS relay sudo python pitmRelay.py
-sleep 1
-sudo screen -r relay
