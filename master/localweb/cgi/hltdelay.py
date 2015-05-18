5720310c15fbc5d4a7d8cf73c92a26cf45b10419
Adam Allen <allena29@users.noreply.github.com>
2015-05-18 22:57:01 +0100

    basic interface to set hlt delay

--- master/localweb/cgi/hltdelay.py
+++ master/localweb/cgi/hltdelay.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python
+import json
+import os
+import time
+import cgi
+
+print "Content-Type: text/plain\n"
+
+updated=False
+hltMode=False
+if os.path.exists("../ipc/swHlt") or os.path.exists("../ipc/manualswitch_swHlt"):
+	hltMode=True
+
+delayUntil=0
+if os.path.exists("../ipc/hlt-delay-until"):
+	try:
+		delayUntil=int(open("../ipc/hlt-delay-until").read())
+	except:
+		pass
+form=cgi.FieldStorage()
+if form.has_key("delay"):
+	updated=True
+	try:
+		delayUntil=time.mktime(time.strptime(form['delay'].value))
+		o=open("../ipc/hlt-delay-until","w")
+		o.write("%s" %(int(delayUntil)))
+		o.close()
+	except:
+		updated=False
+result={'hlt':hltMode,'delayUntil':[delayUntil,time.ctime(delayUntil)] ,'updateValid':updated}
+
+print json.dumps(result)
