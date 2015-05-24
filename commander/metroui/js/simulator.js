bb3b187973437084781d59383ca0489490d943bd
Adam Allen <allena29@users.noreply.github.com>
2015-05-24 22:35:53 +0000

    led flasher in place

--- commander/metroui/js/simulator.js
+++ commander/metroui/js/simulator.js
@@ -9,7 +9,7 @@ if ('WebSocket' in window){
 //var connection = new WebSocket('ws://brewerslab.mellon-collie.net:54662/simulator');
 var connection = new WebSocket('ws://brewerslab.mellon-collie.net:54662/simulator-lcd');
 connection.onmessage = function(e){
-console.log(e.data);
+//console.log(e.data);
 obj = JSON.parse(e.data);
 if(obj.line > -1 ){
 	document.getElementById('lcd'+obj.line).innerHTML=obj.text+"&nbsp;";
@@ -20,6 +20,59 @@ if(obj.line > -1 ){
 
 connection.onclose = function(){
 	alert("Simulator session closed");
+}
+
+
+var connection2 = new WebSocket('ws://brewerslab.mellon-collie.net:54662/simulator-led');
+connection2.onmessage = function(e){
+console.log(e.data);
+obj = JSON.parse(e.data);
+ledFound=false;
+ledColour="";
+ledName="";
+if("lSys" in obj){
+	ledFound=true;
+	ledName="ledSys";
+	ledColour=obj.lSys.colour;
+}
+if("lHlt" in obj){
+	ledFound=true;
+	ledName="ledHlt";
+	ledColour=obj.lHlt.colour;
+}
+if("lSparge" in obj){
+	ledFound=true;
+	ledName="ledSparge";
+	ledColour=obj.lSparge.colour;
+}
+if("lBoil" in obj){
+	ledFound=true;
+	ledName="ledBoil";
+	ledColour=obj.lBoil.colour;
+}
+if("lMash" in obj){
+	ledFound=true;
+	ledName="ledMash";
+	ledColour=obj.lMash.colour;
+}
+if("lFerm" in obj){
+	ledFound=true;
+	ledName="ledFerm";
+	ledColour=obj.lMash.colour;
+}
+
+
+
+
+if(ledFound){
+	console.log("ledFound "+ledName+" "+ledColour);
+	document.getElementById( ledName ).src="/metroui/realtimeview/led"+ledColour+".png";
+}
+ 
+}
+
+connection2.onclose = function(){
+//	alert("Simulator session closed");
 }
 
 
--- commander/websocket/server.py
+++ commander/websocket/server.py
@@ -11,8 +11,10 @@ from pitmCfg import *
 
 cfg=pitmCfg()
 clients = []
-globalData={'/simulator-lcd':[{},{},{},{} ] }
-dataLastUpdate={'/simulator-lcd':0}
+globalData={'/simulator-lcd':[{},{},{},{} ],
+		'/simulator-led':{}  }
+dataLastUpdate={'/simulator-lcd':0,
+		'/simulator-led':0}
 clientLastUpdate={}
 
 def lcdMcast():
@@ -30,15 +32,40 @@ def lcdMcast():
 		try:
 			globalData['/simulator-lcd'][ j['line'] ]  = j
 			dataLastUpdate['/simulator-lcd']=time.time()
-		except ImportError:
+		except:
 			pass
-		time.sleep(0.5)		
+		time.sleep(0.1)		
 
 
 sys.stderr.write("Joining Multicast LCD\n")
 lcdThread = threading.Thread(target=lcdMcast)
 lcdThread.daemon = True
 lcdThread.start()
+
+def ledMcast():
+	global globalData,dataLastUpdate
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	sock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_TTL, 4)
+	sock.bind(('', cfg.mcastFlasherInPort))
+	mreq = struct.pack("4sl", socket.inet_aton(cfg.mcastGroup), socket.INADDR_ANY)
+	sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+	while True:
+		(data, addr) = sock.recvfrom(1200)
+		j=json.loads(data)
+		try:
+			globalData['/simulator-led'] = j
+			dataLastUpdate['/simulator-led']=time.time()
+		except:
+			pass
+		time.sleep(0.3)		
+
+
+sys.stderr.write("Joining Multicast LED\n")
+ledThread = threading.Thread(target=ledMcast)
+ledThread.daemon = True
+ledThread.start()
 
 
 def publishWs():
@@ -53,7 +80,14 @@ def publishWs():
 							sys.stderr.write("%s	%s	%s/%s\n" %(time.time(),client.address,lcdLine['text'],lcdLine['line']))
 							client.sendMessage( u"%s" %( json.dumps(lcdLine)  ))
 
-		time.sleep(1)
+			if client.request.path == "/simulator-led":
+				if dataLastUpdate[ client.request.path ] > clientLastUpdate[ "%s%s" %(client.address) ]:
+					clientLastUpdate[ "%s%s" %(client.address) ] = dataLastUpdate[ client.request.path ]
+					if globalData[ client.request.path ].has_key("_operation"):
+						sys.stderr.write("%s	%s	%s/\n" %(time.time(),client.address,globalData['/simulator-led'] ))
+						client.sendMessage( u"%s" %( json.dumps(globalData['/simulator-led']  )))
+
+		time.sleep(0.3)
 
 
 
