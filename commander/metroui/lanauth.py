f7afb660ef807d30b646d54ceab60074d06c8be7
Adam Allen <allena29@users.noreply.github.com>
2015-05-25 15:51:16 +0000

    local servers added for ssh based access

--- commander/metroui/lanauth.py
+++ commander/metroui/lanauth.py
@@ -4,7 +4,7 @@ import sys
 class lanauth:
 
 	def __init__(self):
-		self.localUsers=['192.168.1.34','192.168.1.36']
+		self.localUsers=['192.168.1.34','192.168.1.36','192.168.1.10','127.0.0.1']
 #		self.localUsers=[]
 
 		self.localUser=False
