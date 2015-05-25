ad9a2be84d7f4b157d5124e6c885629fd1762f3b
Adam Allen <allena29@users.noreply.github.com>
2015-05-25 20:11:20 +0000

    server now has 6 mcast threads

--- commander/websocket/server.py
+++ commander/websocket/server.py
@@ -13,9 +13,15 @@ cfg=pitmCfg()
 clients = []
 globalData={'/simulator-lcd':[{},{},{},{} ],
 		'/simulator-led':{},
+		'/simulator-ssr':{},
+		'/simulator-relay':{},
+		'/simulator-gov':{},
 		'/simulator-button':{}  }
 dataLastUpdate={'/simulator-lcd':0,
 		'/simulator-led':0,
+		'/simulator-ssr':0,
+		'/simulator-relay':0,
+		'/simulator-gov':0,
 		'/simulator-button':0}
 clientLastUpdate={}
 
@@ -75,6 +81,88 @@ ledThread = threading.Thread(target=ledMcast)
 ledThread.daemon = True
 ledThread.start()
 
+
+def relayMcast():
+	global globalData,dataLastUpdate
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	sock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_TTL, 4)
+	sock.bind(('', cfg.mcastRelayPort))
+	mreq = struct.pack("4sl", socket.inet_aton(cfg.mcastGroup), socket.INADDR_ANY)
+	sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+	while True:
+		(data, addr) = sock.recvfrom(1200)
+		j=json.loads(data)
+		try:
+			globalData['/simulator-relay'] = j
+			dataLastUpdate['/simulator-relay']=time.time()
+		except:
+			pass
+		time.sleep(0.8)		
+
+
+sys.stderr.write("Joining Multicast Relay\n")
+relayThread = threading.Thread(target=relayMcast)
+relayThread.daemon = True
+relayThread.start()
+
+
+def ssrrelayMcast():
+	global globalData,dataLastUpdate
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	sock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_TTL, 4)
+	sock.bind(('', cfg.mcastSsrRelayPort))
+	mreq = struct.pack("4sl", socket.inet_aton(cfg.mcastGroup), socket.INADDR_ANY)
+	sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+	while True:
+		(data, addr) = sock.recvfrom(1200)
+		j=json.loads(data)
+		try:
+			globalData['/simulator-ssr'] = j
+			dataLastUpdate['/simulator-ssr']=time.time()
+		except:
+			pass
+		time.sleep(0.2)		
+
+
+sys.stderr.write("Joining Multicast SSR Relay\n")
+ssrrelayThread = threading.Thread(target=ssrrelayMcast)
+ssrrelayThread.daemon = True
+ssrrelayThread.start()
+
+
+
+def govMcast():
+	global globalData,dataLastUpdate
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	sock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_TTL, 4)
+	sock.bind(('', cfg.mcastPort))
+	mreq = struct.pack("4sl", socket.inet_aton(cfg.mcastGroup), socket.INADDR_ANY)
+	sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+	while True:
+		(data, addr) = sock.recvfrom(1200)
+		j=json.loads(data)
+		try:
+			globalData['/simulator-gov'] = j
+			dataLastUpdate['/simulator-gov']=time.time()
+		except:
+			pass
+		time.sleep(0.9)		
+
+
+sys.stderr.write("Joining Multicast Gov\n")
+govThread = threading.Thread(target=govMcast)
+govThread.daemon = True
+govThread.start()
+
+
+
+
 def buttonMcast():
 	global globalData,dataLastUpdate
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -123,6 +211,23 @@ def publishWs():
 					clientLastUpdate[ "%s%s" %(client.address) ] = dataLastUpdate[ client.request.path ]
 					if globalData[ client.request.path ].has_key("_operation"):
 						client.sendMessage( u"%s" %( json.dumps(globalData['/simulator-button']['_button']  )))
+
+			if client.request.path == "/simulator-relay":
+				if dataLastUpdate[ client.request.path ] > clientLastUpdate[ "%s%s" %(client.address) ]:
+					clientLastUpdate[ "%s%s" %(client.address) ] = dataLastUpdate[ client.request.path ]
+					if globalData[ client.request.path ].has_key("_operation"):
+						client.sendMessage( u"%s" %( json.dumps(globalData['/simulator-relay']  )))
+
+			if client.request.path == "/simulator-ssr":
+				if dataLastUpdate[ client.request.path ] > clientLastUpdate[ "%s%s" %(client.address) ]:
+					clientLastUpdate[ "%s%s" %(client.address) ] = dataLastUpdate[ client.request.path ]
+					if globalData[ client.request.path ].has_key("_operation"):
+						client.sendMessage( u"%s" %( json.dumps(globalData['/simulator-ssr']  )))
+			if client.request.path == "/simulator-gov":
+				if dataLastUpdate[ client.request.path ] > clientLastUpdate[ "%s%s" %(client.address) ]:
+					clientLastUpdate[ "%s%s" %(client.address) ] = dataLastUpdate[ client.request.path ]
+					if globalData[ client.request.path ].has_key("_operation"):
+						client.sendMessage( u"%s" %( json.dumps(globalData['/simulator-gov']  )))
 
 		time.sleep(0.3)
 
