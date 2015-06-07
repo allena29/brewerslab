ca1169161e443e09bf30dee1f6222d6b2b7f7539
allena29@users.noreply.github.com <beer@d-i-b8-27-eb-75-f5-a2.home>
2015-06-07 19:38:02 +0100

    localserve update for graph

--- master/localweb/localserve.py
+++ master/localweb/localserve.py
@@ -21,14 +21,7 @@ os.chdir("localweb")
 handler = CGIHTTPServer.CGIHTTPRequestHandler
 server = ThreadingSimpleServer(('', 54661), handler)
 
-# on win32 we are testing just against the directory  for if allowed or not
-# on python 2.6 (linux) we seemed to be testing the whole path
-# CGIHTTPServerAAA is an adapation which works for win32.
 handler.cgi_directories = ["/cgi"]
-#
-#/vsdwba/ofp/prod/merged"]
-#handler.cgi_directories = ["/vsdwba/"]
-#ofp/prod/merged/","/vsdwba/prod/merged/"]
 
 try:
     while 1:
