a9e8d7eda035809262fae58f86ee2a9dc7c3ae8f
Adam Allen <allena29@users.noreply.github.com>
2015-06-07 19:08:33 +0000

    changed frequency of graph-proxy refresh

--- commander/metroui/graph-proxy.py
+++ commander/metroui/graph-proxy.py
@@ -7,7 +7,7 @@ import os
 if os.path.exists("../websocket/mode"):
 	mode=open("../websocket/mode").read()
 	if mode.count("ferm"):
-		sys.stdout.write("Refresh: 300;url=graph-proxy.py?%s\n" %(time.time()))
+		sys.stdout.write("Refresh: 660;url=graph-proxy.py?%s\n" %(time.time()))
 sys.stdout.write("Content-Type: image/png\n\n")
 
 f=urllib.urlopen("http://192.168.1.15:54661/cgi/graph.py")
