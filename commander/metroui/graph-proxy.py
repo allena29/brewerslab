fa29d9976f9f16a04249b941c24eb844c85a1643
Adam Allen <allena29@users.noreply.github.com>
2015-06-07 19:02:11 +0000

    proxy graph from commander to slave to avoid exposing slave to internet

---
+++ commander/metroui/graph-proxy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import time
+import sys
+import urllib
+import os
+
+if os.path.exists("../websocket/mode"):
+	mode=open("../websocket/mode").read()
+	if mode.count("ferm"):
+		sys.stdout.write("Refresh: 300;url=graph-proxy.py?%s\n" %(time.time()))
+sys.stdout.write("Content-Type: image/png\n\n")
+
+f=urllib.urlopen("http://192.168.1.15:54661/cgi/graph.py")
+sys.stdout.write(f.read())
