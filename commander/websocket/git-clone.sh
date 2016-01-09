f7daa30c9bd517232d6d695c12a91230101e84dd
Adam Allen <allena29@noreply.github.com>
2016-01-09 12:58:02 +0000

    working version of simple-web-socket

--- commander/websocket/git-clone.sh
+++ commander/websocket/git-clone.sh
@@ -1,2 +1,6 @@
 git clone https://github.com/dpallot/simple-websocket-server.git
 
+Note: pulled the latest from git 2016 and things broke.
+This commit was originally used 
+https://github.com/dpallot/simple-websocket-server/commit/3f15476d6b8b76a07d31f98df782ff16b68701d0
+
