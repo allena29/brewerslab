30ca441ee4829f3eef12d754f4b3a6aabb54bbf8
Adam Allen <allena29@users.noreply.github.com>
2016-01-12 20:06:43 +0000

    more tweet mode changes. including a change to automatically increase the temperture post-active ferm

--- master/tweetMode.py
+++ master/tweetMode.py
@@ -32,6 +32,7 @@ brewlog=open("ipc/brewlog-id").read().rstrip()
 ##
 
 if not os.path.exists("ipc/swFerm") and os.path.exists("ipc/activityDough") and not os.path.exists("ipc/tweet-grainbill"):
+	print "Checking Grain Bill....",
 	flag=open("ipc/tweet-grainbill","w")
 	flag.close()	
 	con=mysql.connector.connect(host="192.168.1.13",user='brewerslab',password='beer',database="brewerslab")
@@ -40,13 +41,15 @@ if not os.path.exists("ipc/swFerm") and os.path.exists("ipc/activityDough") and 
 	for row in cursor:
 		(recipename,ingredient,qty,unit)=row
 		doTweet("added %s %s of %s #grainbill #brewerslab #%s" %(qty,unit,ingredient,re.compile("[^A-Za-z0-9]").sub('',recipename)))
-
+		print "DONE",
+	print ""
 
 ##
 ## HOPS
 ##
 ## FWH
 if not os.path.exists("ipc/swFerm") and os.path.exists("ipc/activityHltSparge") and not os.path.exists("ipc/swSparge") and not os.path.exists("ipc/tweet-fwh"):
+	print "Checking FWH Hops....",
 	flag=open("ipc/tweet-fwh","w")
 	flag.close()	
 	con=mysql.connector.connect(host="192.168.1.13",user='brewerslab',password='beer',database="brewerslab")
@@ -54,13 +57,14 @@ if not os.path.exists("ipc/swFerm") and os.path.exists("ipc/activityHltSparge") 
 	cursor.execute("select recipe,ingredient,qty,unit,hopAddAt from gIngredients,gBrewlogs where gBrewlogs.recipe=gIngredients.recipename and brewlog='%s' and ingredientType='hops' AND hopAddAt >20 AND hopAddAt <21" %(brewlog))
 	for row in cursor:
 		(recipename,ingredient,qty,unit,hopAddAt)=row
-		print row
 		doTweet("added %s %s of %s First Wort Hops #hopbill #brewerslab #%s" %(qty,unit,ingredient,re.compile("[^A-Za-z0-9]").sub('',recipename)))
-
+		print "DONE",
+	print ""
 
 
 ## COPPER 
 if not os.path.exists("ipc/swFerm") and os.path.exists("activityReachedBoil") and not os.path.exists("ipc/tweet-copperhop"):
+	print "Checking Copper Hops....",
 	flag=open("ipc/tweet-copperhop","w")
 	flag.close()	
 	con=mysql.connector.connect(host="192.168.1.13",user='brewerslab',password='beer',database="brewerslab")
@@ -68,14 +72,15 @@ if not os.path.exists("ipc/swFerm") and os.path.exists("activityReachedBoil") an
 	cursor.execute("select recipe,ingredient,qty,unit,hopAddAt from gIngredients,gBrewlogs where gBrewlogs.recipe=gIngredients.recipename and brewlog='%s' and ingredientType='hops' AND hopAddAt >50" %(brewlog))
 	for row in cursor:
 		(recipename,ingredient,qty,unit,hopAddAt)=row
-		print row
 		doTweet("added %s %s of %s Copper Hops #hopbill #brewerslab #%s" %(qty,unit,ingredient,re.compile("[^A-Za-z0-9]").sub('',recipename)))
-
+		print "DONE",
+	print ""
 
 #
 # Aroma
 #
 if not os.path.exists("ipc/swFerm") and os.path.exists("ipc/activityAromaHops") and os.path.exists("ipc/swBoil") and not os.path.exists("ipc/tweet-aroma"):
+	print "Checking Aroma Hops....",
 	flag=open("ipc/tweet-aroma","w")
 	flag.close()	
 	con=mysql.connector.connect(host="192.168.1.13",user='brewerslab',password='beer',database="brewerslab")
@@ -83,29 +88,31 @@ if not os.path.exists("ipc/swFerm") and os.path.exists("ipc/activityAromaHops") 
 	cursor.execute("select recipe,ingredient,qty,unit,hopAddAt from gIngredients,gBrewlogs where gBrewlogs.recipe=gIngredients.recipename and brewlog='%s' and ingredientType='hops' AND hopAddAt >5 AND hopAddAt < 20" %(brewlog))
 	for row in cursor:
 		(recipename,ingredient,qty,unit,hopAddAt)=row
-		print row
 		doTweet("added %s %s of %s Aroma Hops #hopbill #brewerslab #%s" %(qty,unit,ingredient,re.compile("[^A-Za-z0-9]").sub('',recipename)))
-
+		print "DONE",
+	print ""
 
 #
-# Aroma
+# Finishing Hops
 #
 if not os.path.exists("ipc/swFerm") and os.path.exists("ipc/activityFinishingHops") and os.path.exists("ipc/swBoil") and not os.path.exists("ipc/tweet-finishhop"):
+	print "Checking Finishing Hops....",
 	flag=open("ipc/tweet-finishhop","w")
 	flag.close()	
 	con=mysql.connector.connect(host="192.168.1.13",user='brewerslab',password='beer',database="brewerslab")
 	cursor=con.cursor()
 	cursor.execute("select recipe,ingredient,qty,unit,hopAddAt from gIngredients,gBrewlogs where gBrewlogs.recipe=gIngredients.recipename and brewlog='%s' and ingredientType='hops' AND hopAddAt >3 AND hopAddAt < 10" %(brewlog))
 	for row in cursor:
-		(recipename,ingredient,qty,unit,hopAddAt)=row
-		print row
+		(recipename,ingredient,qty,unit,hopAddAt)=row	
 		doTweet("added %s %s of %s Finishing Hops #hopbill #brewerslab #%s" %(qty,unit,ingredient,re.compile("[^A-Za-z0-9]").sub('',recipename)))
-
+		print "DONE",
+	print ""
 
 #
 # Flameout
 #
 if not os.path.exists("ipc/swFerm") and os.path.exists("ipc/activityFlameoutHops") and not os.path.exists("ipc/swBoil") and not os.path.exists("ipc/tweet-flameout"):
+	print "Checking Flameout Hops....",
 	flag=open("ipc/tweet-flameout","w")
 	flag.close()	
 	con=mysql.connector.connect(host="192.168.1.13",user='brewerslab',password='beer',database="brewerslab")
@@ -113,15 +120,15 @@ if not os.path.exists("ipc/swFerm") and os.path.exists("ipc/activityFlameoutHops
 	cursor.execute("select recipe,ingredient,qty,unit,hopAddAt from gIngredients,gBrewlogs where gBrewlogs.recipe=gIngredients.recipename and brewlog='%s' and ingredientType='hops' AND hopAddAt >0 AND hopAddAt < 0.002" %(brewlog))
 	for row in cursor:
 		(recipename,ingredient,qty,unit,hopAddAt)=row
-		print row
 		doTweet("added %s %s of %s Flameout Hops #hopbill #brewerslab #%s" %(qty,unit,ingredient,re.compile("[^A-Za-z0-9]").sub('',recipename)))
-
-
+		print "DONE"
+	print ""
 
 #
 # Whirlpool
 #
 if not os.path.exists("ipc/swFerm") and os.path.exists("ipc/activityWhirlpool") and not os.path.exists("ipc/tweet-whirlpoolhop"):
+	print "Checking Whirlpool hops....",
 	flag=open("ipc/tweet-whirlpoolhop","w")
 	flag.close()	
 	con=mysql.connector.connect(host="192.168.1.13",user='brewerslab',password='beer',database="brewerslab")
@@ -129,14 +136,14 @@ if not os.path.exists("ipc/swFerm") and os.path.exists("ipc/activityWhirlpool") 
 	cursor.execute("select recipe,ingredient,qty,unit,hopAddAt from gIngredients,gBrewlogs where gBrewlogs.recipe=gIngredients.recipename and brewlog='%s' and ingredientType='hops' AND hopAddAt >0.001 AND hopAddAt < 0.003" %(brewlog))
 	for row in cursor:
 		(recipename,ingredient,qty,unit,hopAddAt)=row
-		print row
 		doTweet("added %s %s of %s Whirlpool hops #hopbill #brewerslab #%s" %(qty,unit,ingredient,re.compile("[^A-Za-z0-9]").sub('',recipename)))
-
-
+		print "DONE",
+	print ""
 #
 # Dry Hop
 #
 if os.path.exists("ipc/swFerm") and os.path.exists("ipc/activityDryhop") and not os.path.exists("ipc/tweet-dryhop"):
+	print "Checking Dry-hop {button press}....",
 	flag=open("ipc/tweet-dryhop","w")
 	flag.close()	
 	con=mysql.connector.connect(host="192.168.1.13",user='brewerslab',password='beer',database="brewerslab")
@@ -145,9 +152,9 @@ if os.path.exists("ipc/swFerm") and os.path.exists("ipc/activityDryhop") and not
 	cursor.execute("select recipe,ingredient,qty,unit,hopAddAt from gIngredients,gBrewlogs where gBrewlogs.recipe=gIngredients.recipename and brewlog='%s' and ingredientType='hops' AND hopAddAt >0.001 AND hopAddAt < 0.003" %(brewlog))
 	for row in cursor:
 		(recipename,ingredient,qty,unit,hopAddAt)=row
-		print row
 		doTweet("added %s %s of %s dry hops #hopbill #brewerslab #%s" %(qty,unit,ingredient,re.compile("[^A-Za-z0-9]").sub('',recipename)))
-
+		print "DONE",
+	print ""
 
 
 
@@ -157,6 +164,7 @@ if os.path.exists("ipc/swFerm") and os.path.exists("ipc/activityDryhop") and not
 #
 # Boil Volume
 if os.path.exists("ipc/swBoil") and not os.path.exists("ipc/tweet-boilvol"):
+	print "Checking Boil Volume....."
 	con=mysql.connector.connect(host="192.168.1.13",user='brewerslab',password='beer',database="brewerslab")
 	cursor=con.cursor()
 	cursor.execute("select gBrewlogs.recipe,fieldVal from gField,gBrewlogs WHERE gField.brewlog='%s' AND gBrewlogs.brewlog = gField.brewlog AND fieldKey='preBoilVolume' and length(fieldVal)>1" %(brewlog))
@@ -166,8 +174,25 @@ if os.path.exists("ipc/swBoil") and not os.path.exists("ipc/tweet-boilvol"):
 		(recipename,fieldVal)=row
 		doTweet("currently boiling %sL #brewerslab #%s" %(fieldVal,re.compile("[^A-Za-z0-9]").sub('',recipename)))
 
+# SG 1..5
+for s in range(5):
+	sg=s+1
+	if os.path.exists("ipc/swFerm") and not os.path.exists("ipc/tweet-sg%s" %(sg)):
+		print "Checking Gravity Sample %s....." %(sg),
+		con=mysql.connector.connect(host="192.168.1.13",user='brewerslab',password='beer',database="brewerslab")
+		cursor=con.cursor()
+		cursor.execute("select gBrewlogs.recipe,fieldVal from gField,gBrewlogs WHERE gField.brewlog='%s' AND gBrewlogs.brewlog = gField.brewlog AND fieldKey='sg%s' and length(fieldVal)>1" %(brewlog,sg))
+		for row in cursor:
+			flag=open("ipc/tweet-sg%s" %(sg),"w")
+			flag.close()	
+			(recipename,fieldVal)=row
+			doTweet(".. measured gravity at %.3f #brewerslab #%s" %(fieldVal,re.compile("[^A-Za-z0-9]").sub('',recipename)))
+			print fieldVal,
+		print ""
+
 # Original Gravity
 if not os.path.exists("ipc/swPump") and  os.path.exists("ipc/swFerm") and not os.path.exists("ipc/tweet-og"):
+	print "Checking Original Gravity....."
 	og=None
 	con=mysql.connector.connect(host="192.168.1.13",user='brewerslab',password='beer',database="brewerslab")
 	cursor=con.cursor()
@@ -185,7 +210,35 @@ if not os.path.exists("ipc/swPump") and  os.path.exists("ipc/swFerm") and not os
 			flag=open("ipc/tweet-og","w")
 			flag.close()	
 			(recipename,fieldVal)=row
-			doTweet("%sL of wort in the fermenter - %.4f OG #brewerslab #%s" %(fieldVal,float(og),re.compile("[^A-Za-z0-9]").sub('',recipename)))
+			doTweet("%sL of wort in the fermenter - %.3f OG #brewerslab #%s" %(fieldVal,float(og),re.compile("[^A-Za-z0-9]").sub('',recipename)))
+
+
+
+# Final Gravity
+if os.path.exists("ipc/post-ferm")  and not os.path.exists("ipc/tweet-abv"):
+	print "Checking Final Gravity.....",
+	fg=None
+	con=mysql.connector.connect(host="192.168.1.13",user='brewerslab',password='beer',database="brewerslab")
+	cursor=con.cursor()
+	cursor.execute("select gBrewlogs.recipe,fieldVal from gField,gBrewlogs WHERE gField.brewlog='%s' AND gBrewlogs.brewlog = gField.brewlog AND fieldKey='__measuredFg_abv' and length(fieldVal)>1" %(brewlog))
+	for row in cursor:
+		(recipename,fieldVal)=row
+		fg=fieldVal
+	if fg: 
+		abv=0
+		con=mysql.connector.connect(host="192.168.1.13",user='brewerslab',password='beer',database="brewerslab")
+		cursor=con.cursor()
+		cursor.execute("select gBrewlogs.recipe,fieldVal from gField,gBrewlogs WHERE gField.brewlog='%s' AND gBrewlogs.brewlog = gField.brewlog AND fieldKey='__abv' and length(fieldVal)>1" %(brewlog))
+		for row in cursor:
+			(recipename,fieldVal)=row
+			flag=open("ipc/tweet-abv","w")
+			flag.close()	
+			(recipename,fieldVal)=row
+			doTweet("Measured FG as %.3f estimated abv %.2f %% #brewerslab #%s" %(fg,abv,re.compile("[^A-Za-z0-9]").sub('',recipename)))
+			print fieldVal,fg,
+	print ""
+
+
 
 
 # Fermentation Over (reads in post-ferm flag)
@@ -248,13 +301,14 @@ if os.path.exists("ipc/swFerm") and not os.path.exists("ipc/tweet-fermover"):
 	# (note: fridge can be used to bring down to pitching temp)
 	# if we haave 33% of heat events we call it pre-fermentation
 
+	print "Checking ferm status (Algoirthim 1)....",
 	if averageCoolEvents == 0:
 		preFerm=True
 		for b in range(len(buckets)-2):		# start considering 3rd buckets onwards
 			if buckets[b+2][2] > 0 and buckets[b+2][3] > 0:	# only consider buckets with valid timestamps
 #				print "Checking bucket ",b+2,time.ctime(float(buckets[b+2][2])),time.ctime(float(buckets[b+2][3]))
 #				print "\t",buckets[b+2][1],averageHeatEvents*.33
-				if buckets[b+2][1] < averageHeatEvents*0.33:
+				if buckets[b+2][1] < averageHeatEvents*0.33 and preFerm:
 #					print "\tNow think we are in active fermentation %s vs %s" %(buckets[b+2][1],averageHeatEvents)
 					preFerm=False
 				elif not preFerm and buckets[b+2][1] > averageHeatEvents*.33:
@@ -263,9 +317,15 @@ if os.path.exists("ipc/swFerm") and not os.path.exists("ipc/tweet-fermover"):
 					if not os.path.exists("ipc/post-ferm"):
 						flag=open("ipc/post-ferm","w")
 						flag.close()
-					
+	print
 
 	if os.path.exists("ipc/post-ferm"):
-		if time.time() > os.stat("ipc/post-ferm").st_mtime + 86400:
+		print "Checking post ferm....",
+		if time.time() > os.stat("ipc/post-ferm").st_mtime + (1.5*86400):
 			doTweet("%s fermentation nearing an end #brewerslab #%s" %(cfg.tweetProgress,re.compile("[^A-Za-z0-9]").sub('',open("ipc/recipe-name").read())) )
-	
+			o=open("ipc/adjustFermTarget","w")
+			o.write("21")
+			o.close()
+			print "YES"
+		else:
+			print "NO"	
