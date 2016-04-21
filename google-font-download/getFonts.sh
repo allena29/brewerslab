5a64e9878eb26915d32bdfe5c843cdacc5e8c42b
allena29@users.noreply.github.com <beer@d-i-b8-27-eb-75-f5-a2.home>
2016-04-22 00:43:45 +0100

    get fonts copy

---
+++ google-font-download/getFonts.sh
@@ -0,0 +1,10 @@
+cd /home/beer/brewerslab/google-font-download
+
+google-font-download/google-font-download "PT Serif"
+google-font-download/google-font-download "Open Sans"
+google-font-download/google-font-download "Open Sans:300"
+google-font-download/google-font-download "Open Sans:700"
+
+cp *.woff /home/beer/brewerslab/slave/localweb/fonts/
+cp /home/beer/brewerslab/slave/localweb/css/metro-local-google-fonts.css /home/beer/brewerslab/slave/localweb/css/metro.css
+
