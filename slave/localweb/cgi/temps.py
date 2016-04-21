bf16e3b8580bc40e51ca45a03c0712cd73ef678c
allena29@users.noreply.github.com <beer@d-i-b8-27-eb-75-f5-a2.home>
2016-04-22 00:46:39 +0100

    metro local google fonts --- alternative css

---
+++ slave/localweb/cgi/temps.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import time
+import os
+
+print """
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="../js/jquery-1.12.0.min.js"></script>
+    <link href="../css/metro.css" rel="stylesheet">
+    <script src="../js/metro.min.js"></script>
+	<link href="../css/metro-icons.css" rel="stylesheet">
+
+<body>
+    <div class="page-content">
+
+	<div class="container">
+  
+	<h1>Graphs</h1>
+	<h3>List last updated: 
+"""
+
+print time.ctime(os.stat("/archivedata/").st_mtime )
+
+print """</h3>"""
+
+print """
+
+
+
+
+<footer class='app-bar fixed-bottom drop-up'>brewerslab - <a href="https://github.com/allena29/brewerslab/"><span class='fg-white'> &nbsp; <span class="mif-github"></span> &nbsp;github.com/allena29/brewerslab</span></a></footer>
+
+</body>
+
+
+
+</html>
+"""
