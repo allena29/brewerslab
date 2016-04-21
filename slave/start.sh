14a45472d104a1a5c9dc8a1e0807f225b1e8dd61
allena29@users.noreply.github.com <beer@d-i-b8-27-eb-75-f5-a2.home>
2016-04-21 20:48:31 +0100

    support for creating hourly graphs in /archivedata

--- slave/start.sh
+++ slave/start.sh
@@ -2,7 +2,10 @@
 
 date >/tmp/slave_bootup
 ping 192.168.1.10 &
-mount 192.168.1.31:/web /web
+
+
+
+
 
 /etc/init.d/rpcbind restart
 
@@ -16,16 +19,29 @@ modprobe w1-gpio
 modprobe w1-therm
 
 chown /var/log/beer -R
+mkdir /web
+mkdir /currentdata
+mkdir /archivedata
+chown beer:beer /web
+chown beer:beer /currentdata
+chown beer:beer /archivedata
+
 
 ntpdate -s uk.pool.ntp.org
 
 echo "Mount Tmpfs"
 mount -t tmpfs -o size=50m tmpfs /currentdata
 
+
+
+###################################
+### LOCAL ENVIRONMENT
 echo "Mount NAS "
 mount -t nfs -o nolock 192.168.1.31:/Transient/piTempMonitor/archivedata /archivedata
-
+mount 192.168.1.31:/web /web
 /etc/init.d/rpcbind restart
+###################################
+
 
 sh temperature.sh "Launching"
 sh ssr.sh "Launching"
