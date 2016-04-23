7a12ef9971391f238425f7c2d7eb0ff730738679
AAA <allena29@noreply.github.com>
2016-04-23 21:54:23 +0000

    launch standalone temperture monitoring - called from the web

---
+++ slave/launch-standalone-temp.sh
@@ -0,0 +1,31 @@
+
+
+
+if [ -f /tmp/standalone-temp-active ]
+then
+
+ echo "alreay doing temp"
+else
+
+	touch /tmp/standalone-temp-active
+
+
+
+	echo "Kill Inidictor we can take care of that ourself"
+	kill `ps -ef | grep gpio23led | head -n 1 | sed -e 's/^\S* *//' | sed -e 's/ .*//'`
+
+
+
+	python /home/beer/brewerslab/gpio23led.py 4 &
+
+
+
+	echo "start grapher"
+
+	cd /home/beer/brewerslab/slave
+	sh grapher.sh "Launching" 
+	sleep 4
+
+	sh temperature.sh "Launching"
+fi
+	exit 0
